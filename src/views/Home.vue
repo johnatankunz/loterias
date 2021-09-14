1d554466af9a4b040f8a34a9c276b03585c90f7f
johnatan <johnatannkunz@gmail.com>
2021-09-14 13:57:26 -0300

    .

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,18 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+         <ion-title id="title">Gerador de 6 dezenas para loterias</ion-title>
       </ion-toolbar>
     </ion-header>
     
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+           <ion-title size="large" id="title">Gerador de 6 dezenas para loterias</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <ion-input type="text" id="dezenas" disabled>{{primeiraDezena}} - {{segundaDezena}} - {{terceiraDezena}} - {{quartaDezena}} - {{quintaDezena}} - {{sextaDezena}}</ion-input>
+    <ion-button expand="block" @click="generated()">Clique aqui para gerar os números da sorte</ion-button>
     </ion-content>
   </ion-page>
 </template>
@@ -33,36 +30,52 @@ export default defineComponent({
     IonPage,
     IonTitle,
     IonToolbar
+  },
+  data(){
+    return{
+      primeiraDezena: '',
+      segundaDezena: '',
+      terceiraDezena: '',
+      quartaDezena: '',
+      quintaDezena: '',
+      sextaDezena: '',
+     
+    }
+  },
+  methods: {
+   
+
+    generated: function(){
+
+      const dezenas = [];
+
+      while (dezenas.length < 6) {
+        const numeroSorteado = Math.floor(Math.random() * 60 + 1).toString();
+
+        if (dezenas.indexOf(numeroSorteado) == -1)
+            dezenas.push(numeroSorteado);
+    }
+     this.primeiraDezena = dezenas[0];
+     this.segundaDezena = dezenas[1];
+     this.terceiraDezena = dezenas[2];
+     this.quartaDezena = dezenas[3];
+     this.quintaDezena = dezenas[4];
+     this.sextaDezena = dezenas[5];
+  
+    }
   }
 });
 </script>
 
 <style scoped>
-#container {
+#title{
+font-size: 25px;
+text-align: center;
+}
+#dezenas{
   text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+  font-size: 50px;
+  margin-left: 35%;
+  margin-right: 35%;
 }
 </style>
